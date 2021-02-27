ed94c53b29be283347a1f961c7d275ab6750f08a
pavelivashutenko <ibvz11@gmail.com>
2021-02-27 21:08:25 +0700

    10 lessons

---
+++ src/Technologies.js
@@ -0,0 +1,14 @@
+const Technologies = () => {
+  return (
+  <div>
+    <ul>
+      <li>CSS</li>
+      <li>HTML</li>
+      <li>JS</li>
+      <li>React</li>
+    </ul>
+  </div>
+  );
+}
+
+export default Technologies;
