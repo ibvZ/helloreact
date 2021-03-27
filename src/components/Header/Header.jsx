31dcb97ae73992c28de67e59902cbaf003920be5
pavelivashutenko <ibvz11@gmail.com>
2021-03-27 16:34:40 +0700

    61 lessons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={s.header}>
       <div className={s.logo}>SOCIAL NETWORK</div>
+      <div className={s.loginBlock}>
+        { props.isAuth ? props.login
+            : <NavLink to={'/login'}>Login</NavLink>}
+      </div>
     </header>
   );
 }
